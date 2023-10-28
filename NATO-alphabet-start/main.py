1379d7b95e47dc520725be5f391e41736c3343d4
JesseKimani <jessekimani21@gmail.com>
2023-10-28 19:05:02 +0300

    second commit

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -2,8 +2,17 @@ import pandas
 
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-data_dict = {row.letter:row.code for (index, row) in df.iterrows()}
+data_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
-name = input("What's your name? ").upper()
-name_codes = [data_dict[letter] for letter in name]
-print(name_codes)
+is_running = True
+
+while is_running:
+    name = input("Enter a word: ").upper()
+
+    try:
+        name_codes = [data_dict[letter] for letter in name]
+    except KeyError:
+        print("Sorry. Only letters in the alphabets please.")
+    else:
+        print(name_codes)
+        is_running = False
